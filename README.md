9905add10987b63a0180665c1949cbdf1d83286d
KIMJH <kjh123qwa@naver.com>
2021-01-08 18:55:30 +0900

    update homework 4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MyHomeWork
 
-### 1. GNGHomeWork1 - ダイスを投げる。
+### 1. gngHomeWork1 - ダイスを投げる。
 
   + packageのタイトル：<b>com.gngHomeWork1</b>
   + ダイスを投げて出る結果をリストで<i><b>retrun</b></i>するメソッドを作成する。
@@ -15,3 +15,8 @@
   + Packageのタイトル：<b>com.gngHomeWork3</b>
   + 1から100までの数字をランダムで出力する。
   + 2020.12.28：SolutionB, SolutionCの追加。Mainの函数にTestの追加。
+
+### 4. gngHomeWork4 - ビンゴをしましょう。
+
+  + Packageのタイトル：<b>com.gngHomeWork4</b>
+  + 今回作成したNumberGenerator100クラスを利用してビンゴをする。
---
+++ src/com/gngHomeWork4/BingoChecker.java
@@ -0,0 +1,87 @@
+package com.gngHomeWork4;
+
+import com.gngHomeWork3.NumberGenerator100;
+
+public class BingoChecker {
+	
+	private byte[][] board5x5;
+	
+	public BingoChecker(byte[][] board5x5) { this.board5x5 = board5x5; }
+	
+	public void PrintBoard() {
+		System.out.println();
+		System.out.println("===============");
+		System.out.println("=[BINGO BOARD]=");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print(String.format("%3d", board5x5[i][j]));
+			}
+			System.out.println();
+		}
+		System.out.println("===============");
+	}
+	
+	/**
+	 * 今回呼ばれた数字で、ビンゴが完成したのかを確認する
+	 * 
+	 * @param calledNum 今回呼ばれた数字
+	 * @return ビンゴが完成したら「true」を返す
+	 */
+	public boolean checkBingo(byte calledNum) {
+		boolean returnValue = false;
+		
+//		번호 찾기 and 번호에 해당되면 0으로 변경
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (board5x5[i][j] == calledNum) board5x5[i][j] = 0;
+			}
+		}
+		
+//		가로, 세로, 대각선 중 합이 0이 된다면 빙고!
+		for (int i = 0; i < 5; i++) {
+			int sumHor = 0; // 가로 줄 체크 용
+			int sumVer = 0; // 세로 줄 체크 용
+			int sumDia1 = 0; // 대각선 체크 용
+			int sumDia2 = 0; // 반대 대각선 체크 용
+			for (int j = 0; j < 5; j++) {
+				sumHor += board5x5[i][j]; // 가로 줄 합
+				sumVer += board5x5[j][i]; // 세로 줄 합
+				if(i == 0) sumDia1 += board5x5[j][j];
+				if(i == 0) sumDia2 += board5x5[4-j][j];
+			}
+			if(sumHor == 0 || sumVer == 0 || (i == 0 && (sumDia1 == 0 || sumDia2 == 0))) {
+				returnValue = true;
+				break;
+			}
+		}
+		
+		return returnValue;
+	}
+	
+	public static void main(String[] args) {
+		final byte[][] sampleBoard = new byte[][] {
+			new byte[] {97,12, 8,14, 2},
+			new byte[] {33,98,87,66,91},
+			new byte[] {23,85,10,30,54},
+			new byte[] {34,38,42,50,46},
+			new byte[] {95,56,63,92,21}
+		};
+		final BingoChecker checker = new BingoChecker(sampleBoard);
+		
+		final NumberGenerator100 numGen = new NumberGenerator100();
+		for (int i = 0; i < 100; i++) {
+			final byte called = numGen.generate('A');
+			
+			if (i % 10 == 0) System.out.println();
+			System.out.print(String.format("%3d", called));
+			
+			if (checker.checkBingo(called)) {
+				System.out.println();
+				checker.PrintBoard();
+				System.out.println("\nBingo!");
+				break;
+			}
+		}
+	}
+
+}
